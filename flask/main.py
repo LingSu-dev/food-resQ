013443ab3893a094539a5327629d23e9c490ee3d
calvin-liew <calvin.y.liew@gmail.com>
2023-10-14 09:18:24 -0400

    added get and post to be implemented

--- flask/main.py
+++ flask/main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os 
+import openai 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/ingredients', methods=["GET"])
+def get_ingredient():
+    pass
+
+@app.route('/post', methods=["POST"])
+def ask_recipe():
+    pass
 
 
 app.run(host='0.0.0.0', port=81)
