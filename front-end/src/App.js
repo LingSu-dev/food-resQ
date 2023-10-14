26fe4721efc26b2efc820d8a699cbe441d0c5393
frank-skyblue <su.kong@mail.utoronto.ca>
2023-10-14 16:24:58 -0400

    Finished Home

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,14 @@ import './App.scss';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/sign-in";
 import SignUp from "./pages/sign-up";
-
+import Home from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
+            <Route index element={<Home />}/>
             <Route path="signin" element={<Signin />}/>
             <Route path="signup" element={<SignUp />}/>
         </Route>
